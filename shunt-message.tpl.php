d46f4ba93e03cc2cc22dccbd528aac8673d91d84
Matthew Grasmick <matthew.grasmick@acquia.com>
2014-07-25 12:25:02 -0400

    Adding new default shunts.

---
+++ shunt-message.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * This template handles the display of the shunt message.
+ *
+ * Variables available:
+ * - $message: The message to display
+ */
+
+?>
+<div class="shunt-message">
+  <p><?php print $message; ?></p>
+</div>
